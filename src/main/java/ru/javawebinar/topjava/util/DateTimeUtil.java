89cbbb0cd892fabf633f932dbc8cfd304ef38967
Igor Isakov <igris198@gmail.com>
2025-02-21 11:59:46 +0700

    3_03_HW2_meal_layers

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.util;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -9,6 +10,18 @@ public class DateTimeUtil {
 
     public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
         return !lt.isBefore(startTime) && lt.isBefore(endTime);
+    }
+
+    // DB doesn't support LocalDate.MIN/MAX
+    private static final LocalDateTime MIN_DATE = LocalDateTime.of(1, 1, 1, 0, 0);
+    private static final LocalDateTime MAX_DATE = LocalDateTime.of(3000, 1, 1, 0, 0);
+
+    public static LocalDateTime atStartOfDayOrMin(LocalDate localDate) {
+        return localDate != null ? localDate.atStartOfDay() : MIN_DATE;
+    }
+
+    public static LocalDateTime atStartOfNextDayOrMax(LocalDate localDate) {
+        return localDate != null ? localDate.plusDays(1).atStartOfDay() : MAX_DATE;
     }
 
     public static String toString(LocalDateTime ldt) {
